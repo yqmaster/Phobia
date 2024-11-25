c99e47677151e4f24e537def1d0eccc55742e1e7
花火 <2904276237@qq.com>
2024-11-25 14:06:30 +0000

    feat: PArrowComponent 抽取到插件

--- Plugins/GameFeatures/BaseClass/Source/BaseClass/Public/Components/ArrowComponentBase.h
+++ Plugins/GameFeatures/BaseClass/Source/BaseClass/Public/Components/ArrowComponentBase.h
@@ -2,13 +2,13 @@
 
 #include "CoreMinimal.h"
 #include "Components/ArrowComponent.h"
-#include "PArrowComponent.generated.h"
+#include "ArrowComponentBase.generated.h"
 
 /**
  * 
  */
 UCLASS(Blueprintable, Abstract, hidecategories=(Object,LOD,Physics,Lighting,TextureStreaming,Activation,"Components|Activation",Collision), editinlinenew)
-class PHOBIA_API UPArrowComponent : public UArrowComponent
+class BASECLASS_API UArrowComponentBase : public UArrowComponent
 {
 	GENERATED_BODY()
 };
